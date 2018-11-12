81cde0f959899ef213c3709aa12166fb1f1ca91a
Vratislav Bartonicek <vratislav@vbartonicek.cz>
2018-11-12 16:36:29 +0100

    Dwellings triplified

--- download/triplifier/triplifier.sh
+++ download/triplifier/triplifier.sh
@@ -5,10 +5,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OUTPUT=${DIR}/../output
 SOURCE=${DIR}/../../source
 
-
-
-
-
 # DB server address
 DB_SERVER = mysql://127.0.0.1
 
@@ -24,27 +20,31 @@ DB_PASSWORD = ''
 # TODO: http://d2rq.org commands path
 D2RQ_SERVER = http://localhost:2020/
 
-# Dwellings
+
+# Municipalities
 # ---------
 
 # Generate a mapping file for your database schema using the generate-mapping tool. Change into the D2R Server directory and run
-sudo ./generate-mapping -o dwellings-mapping.ttl -u ${DB_USER} -p ${DB_PASSWORD} jdbc:${DB_SERVER}{}${DB_NAME}
+sudo ./generate-mapping -o municipalities-mapping.ttl -u ${DB_USER} -p ${DB_PASSWORD} jdbc:${DB_SERVER}{}${DB_NAME}
 
 # Start D2R Server
 d2r-server mapping.ttl
 
 # Generate an RDF dump using the dump-rdf tool and a mapping file
-sudo ./dump-rdf -f N-TRIPLE -b ${D2RQ_SERVER} dwellings-mapping.ttl > ${OUTPUT}/dwellings.nt
+sudo ./dump-rdf -f N-TRIPLE -b ${D2RQ_SERVER} municipalities-mapping.ttl > ${OUTPUT}/municipalities.nt
 
 
-# Municipalities
+# Dwellings
 # ---------
 
-# TODO
+tarql -d ";" ${DIR}/dwellings.sparql ${SOURCE}/dwellings.csv > ${OUTPUT}/dwellings.ttl
+tarql -d ";" --ntriples ${DIR}/dwellings.sparql ${SOURCE}/dwellings.csv > ${OUTPUT}/dwellings.nt
 
 
 # Population
 # ---------
+
+# TODO: Add transformation from .xlxs to .csv
 
 tarql -d ";" ${DIR}/population.sparql ${SOURCE}/population.csv > ${OUTPUT}/population.ttl
 tarql -d ";" --ntriples ${DIR}/population.sparql ${SOURCE}/population.csv > ${OUTPUT}/population.nt
